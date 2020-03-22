ab52fabc1815665eb96439f0eb03120a072f7b3e
Abdurahman Almonajed <abdulrahman.almonajed@gmail.com>
2020-03-22 15:31:11 +0100

    neke male izmjene

---
+++ assets/js/main.js
@@ -0,0 +1,106 @@
+(function ($) {
+    "use strict";
+
+    $('#search').hide()
+
+    $(function () {
+        var header = $(".start-style");
+        $(window).scroll(function () {
+            var scroll = $(window).scrollTop();
+
+            if (scroll >= 10) {
+                header.removeClass('start-style').addClass("scroll-on");
+            } else {
+                header.removeClass("scroll-on").addClass('start-style');
+            }
+        });
+    });
+
+    //Animation
+
+    $(document).ready(function () {
+        $('body.hero-anime').removeClass('hero-anime');
+    });
+
+    //Menu On Hover
+
+    $('body').on('mouseenter mouseleave', '.nav-item', function (e) {
+        if ($(window).width() > 750) {
+            var _d = $(e.target).closest('.nav-item');
+            _d.addClass('show');
+            setTimeout(function () {
+                _d[_d.is(':hover') ? 'addClass' : 'removeClass']('show');
+            }, 1);
+        }
+    });
+
+    $('#searchIcon').click(() => {
+        //moze bolji nacin al za sad nek bude ovako
+        if ($('#searchIcon').hasClass("fa-search")) {
+            $('#search').show(500)
+            $('#searchIcon').removeClass("fa-search").addClass("fa-search-minus")
+        } else {
+            $('#search').hide(500)
+            $('#searchIcon').removeClass("fa-search-minus").addClass("fa-search")
+        }
+
+    })
+
+    /*====== Price Slider Active ======*/
+    $('#slider-range').slider({
+        range: true,
+        min: 10,
+        max: 500,
+        values: [110, 400],
+        slide: function (event, ui) {
+            $('#amount').val('$' + ui.values[0] + ' - $' + ui.values[1]);
+        }
+    });
+    $('#amount').val('$' + $('#slider-range').slider('values', 0) +
+        " - $" + $('#slider-range').slider('values', 1));
+
+
+})(jQuery);
+
+///// Section-1 CSS-Slider /////    
+// Auto Switching Images for CSS-Slider
+function bannerSwitcher() {
+    next = $('.sec-1-input').filter(':checked').next('.sec-1-input');
+    if (next.length) next.prop('checked', true);
+    else $('.sec-1-input').first().prop('checked', true);
+}
+
+var bannerTimer = setInterval(bannerSwitcher, 5000);
+
+$('nav .controls label').click(function () {
+    clearInterval(bannerTimer);
+    bannerTimer = setInterval(bannerSwitcher, 5000)
+});
+
+//Switch images
+const activeImage = document.querySelector(".product-image .active");
+const productImages = document.querySelectorAll(".image-list img");
+const navItem = document.querySelector('a.toggle-nav');
+
+function changeImage(e) {
+    activeImage.src = e.target.src;
+}
+
+function toggleNavigation() {
+    this.nextElementSibling.classList.toggle('active');
+}
+
+productImages.forEach(image => image.addEventListener("click", changeImage));
+navItem.addEventListener('click', toggleNavigation);
+
+
+///// Anchor Smooth Scroll /////
+//   $('.main-menu a, .learn-more-button a').click(function(e){
+
+//     e.preventDefault();
+
+//     var target = $(this).attr('href');
+
+//     $('html, body').animate({scrollTop: $(target).offset().top}, 1000);
+//     return false;
+//   });
