89cf2244f089fb537fa5bcf5423f7104dff19fe5
Abdurahman Almonajed <abdulrahman.almonajed@gmail.com>
2020-03-24 17:39:56 +0100

    tooltip na mapi
    za kantone

--- assets/js/main.js
+++ assets/js/main.js
@@ -77,27 +77,6 @@
 
 })(jQuery);
 
-//Google maps
-// Initialize and add the map
-/* function initMap() {
-    // The location of Uluru
-    var uluru = {
-        lat: 43.84864,
-        lng: 18.35644
-    };
-    // The map, centered at Uluru
-    var map = new google.maps.Map(
-        document.getElementById('map'), {
-            zoom: 8,
-            center: uluru
-        });
-    // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({
-        position: uluru,
-        map: map
-    });
-} */
-
 //Call numbers
 function citys(value) {
     if (value == "Sarajevo") {
