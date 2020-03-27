4922956c965655bdac830c3b0723061b2b7e89fe
AldinBe <aldinbecic1996@gmail.com>
2020-03-27 17:23:53 +0100

    prelose

--- php/crud.php
+++ php/crud.php
@@ -2,6 +2,8 @@
 
 session_start();
 $mysqli = new mysqli('localhost', 'root', '', 'bih-korona') or die(mysqli_error($mysqli));
+mysqli_set_charset($mysqli, "UTF8");
+
 
 $id = 0;
 $update = false;
@@ -56,6 +58,57 @@ if(isset($_POST['update'])){
     $lfVrijeme = $_POST['vrijeme'];
 
     $mysqli->query("UPDATE live_feed SET lfImeGrada='$lfImeGrada' , lfBroj = '$lfBroj', lfParagraf = '$lfParagraf', vrijeme = '$vrijeme' WHERE id=$id") or die($mysqli->error());
+
+    $_SESSION['message'] = "Update";
+    $_SESSION['msg_type'] = "warning";
+
+    header('Location: ../insertData.php');
+
+}
+
+//Gradovi
+$ime_grada = '';
+$broj_zaraznih = '';
+
+//ADD
+if(isset($_POST['saveG'])){
+    $ime_grada = $_POST['ime_grada'];
+    $broj_zaraznih = $_POST['broj_zaraznih'];
+
+    $mysqli->query("INSERT INTO gradovi (ime_grada,broj_zaraznih) VALUES ('$ime_grada', '$broj_zaraznih')") or die($mysqli->error);
+
+    $_SESSION['message'] = "Sačuvano";
+    $_SESSION['msg_type'] = "success";
+
+    header('Location: ../insertData.php');
+}
+//DELETE
+if(isset($_GET['delete'])){
+    $id = $_GET['delete'];
+    $mysqli->query("DELETE from gradovi WHERE id=$id") or die($mysqli->error());
+
+    $_SESSION['message'] = "Izbrisano";
+    $_SESSION['msg_type'] = "danger";
+
+    header('Location: ../insertData.php');
+}
+//EDDIT
+if(isset($_GET['editG'])){
+    $id = $_GET['editG'];
+    $update = true;
+    $result =  $mysqli->query("SELECT * from gradovi WHERE id=$id") or die($mysqli->error());
+    if(count(array($result))==1){
+        $row = $result->fetch_array();
+        $ime_grada = $row['ime_grada'];
+        $broj_zaraznih = $row['broj_zaraznih'];
+    }
+}
+if(isset($_POST['updateG'])){
+    $id= $_POST['id'];
+    $ime_grada = $_POST['ime_grada'];
+    $broj_zaraznih = $_POST['broj_zaraznih'];
+
+    $mysqli->query("UPDATE gradovi SET ime_grada='$ime_grada' , broj_zaraznih = '$broj_zaraznih' WHERE id=$id") or die($mysqli->error());
 
     $_SESSION['message'] = "Update";
     $_SESSION['msg_type'] = "warning";
