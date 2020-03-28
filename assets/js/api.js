e0750a33a26254009f8a7a4abfb7a7ff26adf99e
Abdurahman Almonajed <abdulrahman.almonajed@gmail.com>
2020-03-28 15:11:13 +0100

    za nove slucajeeve

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,31 +1,43 @@
 fetch('https://thevirustracker.com/free-api?countryTotal=BA')
   .then((response) => {
     return response.json();
-    console.log('juhu')
   })
   .then((data) => {
-    console.log('abu')
     document.getElementById('title-n').innerText = "COVID-19 Statistika - Bosna i Hercegovina";
 
-    document.getElementById('total').innerHTML = `<div class="stats">
-      <div class="number">${data.countrydata[0].total_cases}</div>
+    if (data.countrydata[0].total_new_cases_today > 0) {
+      document.getElementById('total').innerHTML = `<div class="stats">
+      <div class="number">${data.countrydata[0].total_cases} <small> (+${data.countrydata[0].total_new_cases_today} )</small></div>
       <div class="factor text-secondary pt-2">Zaraženih</div>
-    </div>`;
+      </div>`;
+    } else {
+      document.getElementById('total').innerHTML = `<div class="stats">
+      <div class="number">${data.countrydata[0].total_cases} <small> (${data.countrydata[0].total_new_cases_today} )</small></div>
+      <div class="factor text-secondary pt-2">Zaraženih</div>
+      </div>`;
+    }
 
     document.getElementById('recover').innerHTML = `<div class="stats">
-      <div class="number">${data.countrydata[0].total_recovered}</div>
+      <div class="number">${data.countrydata[0].total_recovered}</small></div>
       <div class="factor text-success pt-2">Oporavljenih</div>
     </div>`;
 
     document.getElementById('sick').innerHTML = `<div class="stats">
-      <div class="number">${data.countrydata[0].total_active_cases}</div>
+      <div class="number">${data.countrydata[0].total_active_cases}</small></div>
       <div class="factor text-dark pt-2">Bolesnih</div>
     </div>`;
 
-    document.getElementById('dead').innerHTML = `<div class="stats">
-      <div class="number">${data.countrydata[0].total_deaths}</div>
+    if (data.countrydata[0].total_new_deaths_today > 0) {
+      document.getElementById('dead').innerHTML = `<div class="stats">
+      <div class="number">${data.countrydata[0].total_deaths} <small> (+${data.countrydata[0].total_new_deaths_today} )</small></div>
       <div class="factor text-danger pt-2">Mrtvih</div>
-    </div>`;
+      </div>`;
+    } else {
+      document.getElementById('dead').innerHTML = `<div class="stats">
+      <div class="number">${data.countrydata[0].total_deaths} <small> (${data.countrydata[0].total_new_deaths_today} )</small></div>
+      <div class="factor text-danger pt-2">Mrtvih</div>
+      </div>`;
+    }
   });
 
 
